ddc375509e8163dbb17f62ba03a31fd62e95faa2
Jonlhuang <jonalhuang@gmail.com>
2023-05-04 11:46:06 -0700

    adding table to shiny

--- final_project/app.R
+++ final_project/app.R
@@ -4,9 +4,10 @@ library(tidyverse)
 library(here)
 library(leaflet)
 library(shiny)
+library(DT)
 
-#Load data
-#load data
+#####----------Load data---------####
+
 transect <- read_csv(here("data","transect_code.csv")) %>% 
   drop_na() %>% 
   separate(col = transect_code,
@@ -31,7 +32,25 @@ wnr <- transect %>% filter(reef_name%in% "WNR")
 bk <- transect %>% filter(reef_name%in% "BK")
 smk <- transect %>% filter(reef_name%in% "SMK")
 
-#Make interactive map with Leaflet package
+
+#add fish data
+fish <- read_csv(here("data","songs_clean.csv")) 
+
+#clean dataset
+songs_count <- fish %>% 
+  group_by( year,total_length, species_code, reef_code) %>%  #group analysis 
+  mutate(species_code = as_factor(factor(species_code,   #reorder the factors
+                                         levels = c( "SEPU","PACL","PANE","EMJA","CHPU","OXCA"))),
+         total_length = as.numeric(total_length), #change length to numeric
+         reef_code = as_factor(reef_code),
+         year = as.numeric(year)) %>%    
+  summarise(count = sum(count), #sum the count through the years
+            total_area_sampled = sum(total_area_sampled)) %>%    
+  uncount(count) %>%                   #reorder so each row = 1 count
+  select(year, species_code,reef_code,total_length,total_area_sampled) #reorder df order
+
+
+####------Make interactive map with Leaflet package------#####
 # Create a color palette with handmade bins.
 mybins <- c("WNR","BK","SMK")
 myphase <- c("1","2","3")
@@ -40,10 +59,9 @@ mypalette <- colorFactor( palette=c("green","aquamarine","darkgreen"),
                           na.color="transparent") # if not on color palette, make transparent
 
 
-
-#create text box to place in 
+#create text box to place in map descriptor
 wnrtext <- paste(
-  "Longitude: ", wnr$Longitude, "<br/>", 
+  "Longitude: ", wnr$Longitude, "<br/>", #Br/ = break/next row
   "Latitude: ", wnr$Latitude, "<br/>",
   "Transect code: ",wnr$transect_code, "<br/>",
   "Transect full name: ", wnr$Transect, sep="","<br/>",
@@ -66,6 +84,9 @@ smktext <- paste(
   "Phase built: ", smk$phase_built_code) %>%
   lapply(htmltools::HTML)
 
+
+
+
 ################################################
 ##############  BUILD SHINY  ###################
 ################################################
@@ -74,16 +95,44 @@ ui <- fluidPage(
   textInput(inputId = "choice",
              label = "SONGS Dive sites",
              value = "All Survey Sites"),
-  leafletOutput("map")
-  #select the output type
+  leafletOutput("map"),
+  
+  
+  
+  #Add selection slide 
+  checkboxGroupInput("species_code", label = h3("Select Species"), 
+                     choices = list("SEPU" ="SEPU", "OXCA"="OXCA",
+                                    "CHPU"="CHPU", "PACL"="PACL", 
+                                    "PANE" = "PANE", "EMJA" = "EMJA"),
+                     selected = "SEPU"),
+  # hr(),
+  # fluidRow(column(3, verbatimTextOutput("value")))
+  
+  # Make space for a table
+  h2("Data Table"),
+  DT::dataTableOutput("datatable"),
+  
+  # making space for density plot 
+  plotOutput("density")
+  
   )
 
 
 
 
 server <- function(input, output){
+  
+  data<-reactive({
+    tibble(songs_count %>%
+             filter(species_code %in% c(input$species_code))) #make dataset own object that is reactive everytime num is changed
+  })
+  
+  
+  #####-----Add text----####  
+  #Output for printing text for map
   output$value <- renderPrint({input$choice})
   
+  ####-----add leaflet map to input-----
   output$map <- renderLeaflet(
     leaflet() %>% 
       addTiles()  %>% 
@@ -127,8 +176,69 @@ server <- function(input, output){
       addLayersControl(overlayGroups = c(mybins), 
                        options = layersControlOptions(collapsed = FALSE)) 
   )
+
+  ####-----Print Data Table-----
+  output$datatable = DT::renderDataTable({
+    fish %>% 
+      group_by( year,total_length, species_code, reef_code) %>%  #group analysis 
+      mutate(total_length = as.numeric(total_length), #change length to numeric
+             reef_code = as_factor(reef_code),
+             year = as_factor(year)) %>%   
+      summarise(count = sum(count)) %>% 
+      filter(species_code %in% c(input$species_code))
+  })
+  
+  
+  #####-----Add Checkbox----####  
+  #Print check box to select species you want to look at
+  output$value <- renderPrint({ input$species_code })
+  
+  
+  #####-----Print Density plot----####  
+  output$density <- renderPlot({
+    data() %>% 
+      # filter(reef_code%in% c("BK","SMK")) %>% 
+      mutate(year = as_factor(year)) %>% 
+      ggplot()+   #specify x & solor by year
+      geom_density(
+        aes(x = total_length, color = year, group = year),linewidth = 1.5)+   #use density plot
+      facet_wrap(~species_code, ncol=1, scales = "free")+ #separate plots by species
+      ggh4x::facetted_pos_scales(x = NULL, y = list( #custom scales for each facet
+        SEPU = scale_y_continuous(limits = c(0,0.075)),
+        PACL = scale_y_continuous(limits = c(0,0.15)),
+        PANE = scale_y_continuous(limits = c(0,0.24)),
+        EMJA = scale_y_continuous(limits = c(0,0.15)),
+        CHPU = scale_y_continuous(limits = c(0,0.9)),
+        OXCA = scale_y_continuous(limits = c(0,1.0))
+      ))+
+      scale_x_continuous(breaks = seq(0,max(fish$total_length), 10),
+                         limits = c(0,NA))+  #scale x to be max lenght
+      labs(x = "Total Length (mm)",    #edit axis & legend labels
+           y = "Density", 
+           color = "Year",
+           title = element_text("Size distribution on Natural Reefs (BK & SMK)"))+
+      # scale_color_viridis_d()+  #color palatte
+      scale_fill_manual( values = c("#440154","#481f70","#443983",
+                                             "#3b528b","#31688e","#287c8e",
+                                             "#21918c","#20a486", "#35b779",
+                                             "#5ec962", "#90d743","#c8e020",
+                                             "#fde725"),
+                                             aesthetics = c("color", "fill"),
+                         name = "Year")+
+      theme_classic()+
+      theme(panel.background = element_rect(fill = "grey90", color = "black"),
+            plot.title = element_text(hjust = 0.5, size = 40),
+            axis.text = element_text(size = 30),
+            axis.title = element_text(size = 17),
+            strip.text = element_text(size = 30),
+            legend.text = element_text(size = 30),
+            legend.key.size = unit(1.5,"cm"),
+            legend.title = element_text(size = 30))
+    
+  }, height = 750, width = 750)
+  
 }
 
 
-# Run the application 
+####----- Run the application ----####
 shinyApp(ui = ui, server = server)
--- script/data_wrangle_graphing.R
+++ script/data_wrangle_graphing.R
@@ -32,3 +32,93 @@ songs_count_sum <- fish %>%
 ) 
 
 print(songs_count_sum)
+
+
+#graphing multiple species on a panel to look over time
+p1 <- ggplot(songs_count %>% filter(reef_code%in% "WNR") %>% 
+               mutate(year = as_factor(year)),    #data 
+             aes(x = total_length, color = year))+   #specify x & solor by year
+  geom_density(linewidth = 1.5)+   #use density plot
+  facet_wrap(~species_code,ncol=1, scales = "free")+    #separate plots by species
+  ggh4x::facetted_pos_scales(x = NULL, y = list( #custom scales for each facet
+    SEPU = scale_y_continuous(limits = c(0,0.075)),
+    PACL = scale_y_continuous(limits = c(0,0.15)),
+    PANE = scale_y_continuous(limits = c(0,0.24)),
+    EMJA = scale_y_continuous(limits = c(0,0.15)),
+    CHPU = scale_y_continuous(limits = c(0,0.9)),
+    OXCA = scale_y_continuous(limits = c(0,1.0))
+  ))+
+  scale_x_continuous(breaks = seq(0,max(fish$total_length), 10),
+                     limits = c(0,NA))+  #scale x to be max lenght at increment of 5cm
+  labs(x = "Total Length (mm)",    #edit axis & legend labels
+       y = "Density", 
+       color = "Year",
+       title = element_text("Size distribution on Artifical Reef (WNR)"))+
+  scale_color_viridis_d()+  #color palatte
+  theme_classic()+
+  theme(panel.background = element_rect(fill = "grey90", color = "black"),
+        plot.title = element_text(hjust = 0.5, size = 40),
+        axis.text = element_text(size = 30),
+        axis.title = element_text(size = 17),
+        strip.text = element_text(size = 30),
+        legend.text = element_text(size = 30),
+        legend.key.size = unit(1.5,"cm"),
+        legend.title = element_text(size = 50)) #remove lengend for ggarrange
+p1
+# ggsave(here("output", "size_densitydistribution_WNR.pdf"),
+#        width = 20, height = 10)
+
+p2 <- songs_count %>% filter(reef_code%in% c("BK","SMK")) %>% 
+  mutate(year = as_factor(year)) %>% 
+  ggplot()+   #specify x & solor by year
+  geom_density(
+    aes(x = total_length, color = year, group = year),linewidth = 1.5)+   #use density plot
+  facet_wrap(~species_code, ncol=1, scales = "free")+ #separate plots by species
+  ggh4x::facetted_pos_scales(x = NULL, y = list( #custom scales for each facet
+    SEPU = scale_y_continuous(limits = c(0,0.075)),
+    PACL = scale_y_continuous(limits = c(0,0.15)),
+    PANE = scale_y_continuous(limits = c(0,0.24)),
+    EMJA = scale_y_continuous(limits = c(0,0.15)),
+    CHPU = scale_y_continuous(limits = c(0,0.9)),
+    OXCA = scale_y_continuous(limits = c(0,1.0))
+  ))+
+  scale_x_continuous(breaks = seq(0,max(fish$total_length), 10),
+                     limits = c(0,NA))+  #scale x to be max lenght
+  labs(x = "Total Length (mm)",    #edit axis & legend labels
+       y = "Density", 
+       color = "Year",
+       title = element_text("Size distribution on Natural Reefs (BK & SMK)"))+
+  # scale_color_viridis_d()+  #color palatte
+  scale_fill_manual( values = c("#440154","#481f70","#443983",
+                                         "#3b528b","#31688e","#287c8e",
+                                         "#21918c","#20a486", "#35b779",
+                                         "#5ec962", "#90d743","#c8e020",
+                                         "#fde725"),
+                                         aesthetics = c("color", "fill"),
+                     name = "Year")+
+  theme_classic()+
+  theme(panel.background = element_rect(fill = "grey90", color = "black"),
+        plot.title = element_text(hjust = 0.5, size = 40),
+        axis.text = element_text(size = 30),
+        axis.title = element_text(size = 17),
+        strip.text = element_text(size = 30),
+        legend.text = element_text(size = 30),
+        legend.key.size = unit(1.5,"cm"),
+        legend.title = element_text(size = 50))
+
+p2
+# ggsave(here("output", "size_densitydistribution_BK_SMK.pdf"),
+#        width = 20, height = 10)
+
+# COMBINE THE PLOTS TO HAVE ONE PLOT
+arrange <- ggarrange((p1+rremove("xlab")+rremove("ylab")+rremove("legend"))+(p2+rremove("ylab")+rremove("xlab")),
+                     labels = NULL,
+                     common.legend = TRUE,
+                     legend = "right")
+annotate_figure(arrange, 
+                left = text_grob("Density", #add common axis
+                                 rot = 90, 
+                                 size = 45),
+                bottom = text_grob("Total Length (mm)",
+                                   size = 45))+
+  theme(plot.margin = margin(1,1,1,1, "cm"))
